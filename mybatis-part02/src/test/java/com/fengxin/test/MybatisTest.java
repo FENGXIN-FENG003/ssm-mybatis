466857a3f37dbf51a04ec1e09e6f8c2758429013
枫 <fx20031215@163.com>
2024-07-30 17:13:13 +0800

    feat: 添加#{}和${}的解释和测试

---
+++ mybatis-part02/src/test/java/com/fengxin/test/MybatisTest.java
@@ -0,0 +1,35 @@
+package com.fengxin.test;
+
+import com.fengxin.mapper.EmployeeMapper;
+import com.fengxin.pojo.Employee;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author FENGXIN
+ * @date 2024/7/30
+ **/
+public class MybatisTest {
+    @Test
+    public void test01() throws IOException {
+        // 配置流
+        InputStream is = Resources.getResourceAsStream ("mybatis/MybatisConfig.xml");
+        // 工厂
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder ().build (is);
+        // 会话
+        SqlSession session = sqlSessionFactory.openSession ();
+        // 映射
+        EmployeeMapper employeeMapper = session.getMapper (EmployeeMapper.class);
+        // 根据代理对象调用方法
+        Employee employee = employeeMapper.queryById (1);
+        System.out.println ("employee = " + employee);
+        // 关闭
+        session.close ();
+    }
+}
