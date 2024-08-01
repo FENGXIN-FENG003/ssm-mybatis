394e7fe8edc52a0a6763e056f95c83bd36c788e8
枫 <fx20031215@163.com>
2024-08-01 22:16:31 +0800

    feat: 添加了批量操作和动态SQL功能

--- mybatis-part06-dyn/src/main/java/com/fengxin/mapper/EmployeeMapper.java
+++ mybatis-part06-dyn/src/main/java/com/fengxin/mapper/EmployeeMapper.java
@@ -11,4 +11,16 @@ import java.util.List;
  **/
 public interface EmployeeMapper {
     List<Employee> query(@Param (value = "empName") String name,@Param (value = "empSalary") Double salary);
+    List<Employee> queryTrim(@Param (value = "empName") String name,@Param (value = "empSalary") Double salary);
+    List<Employee> queryChoose(@Param (value = "empName") String name,@Param (value = "empSalary") Double salary);
+    
+    int update(Employee employee);
+    
+    // 批量
+    List<Employee> queryBacth(List<Integer> ids);
+    List<Integer> deleteBacth(List<Integer> ids);
+    int insertBacth(List<Employee> employees);
+    int updateBacth(List<Employee> employees);
+    
+    
 }
